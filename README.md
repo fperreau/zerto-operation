1df4ceb7b08065dbeba0665b975485ba3052b072
Frederic Perreau <Frederic.Perreau@kyndryl.com>
2025-06-30 16:41:03 +0200

    initial zerto usage meter

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # zerto-operation
+##
+
+# prerequisite
+python -m venv .venv
+pip install -r requirements.txt
+
+# execute zerto_usage.py
+.venv\Scripts\activate
+python .\zerto_usage.py -m 5 202505_Phoenix_UsageReport.zip BRSERM_MOP_202505.zip BRSK24_MOP_202505.zip Cegid_report_May2025.zip
+p
---
+++ zerto_usage.py
@@ -0,0 +1,136 @@
+import pandas as pd
+from datetime import datetime as dt
+import argparse as ap
+import regex as re
+import os
+from zipfile import ZipFile
+
+billing_file = "ZertoBilling.zip"
+format_date = "%m/%d/%Y%H:%M:%S%p"
+
+def browse_csv_file(file, zip_file, days, max, count):
+
+    csv_days = [0]*32
+    csv_max = -1
+
+    df = pd.read_csv(file, skiprows=1, usecols=[" VM (Unique ID)", " From Date", " To Date", " Total Days"])
+    df.rename(columns={' VM (Unique ID)': 'VM_Unique_ID'}, inplace=True)
+    df.rename(columns={' From Date': 'From_Date'}, inplace=True)
+    df.rename(columns={' To Date': 'To_Date'}, inplace=True)
+    df.rename(columns={' Total Days': 'Total_Days'}, inplace=True)
+ 
+    csv_count = 0
+    for row in df.itertuples():
+        dFrom = dt.strptime(re.sub('[ ?]','',row.From_Date),format_date)
+        dTo = dt.strptime(re.sub('[ ?]','',row.To_Date),format_date)
+
+        for d in range(dFrom.day,dTo.day+1):
+            days[d-1] += 1
+            csv_days[d-1] += 1
+
+        csv_count += 1
+
+    for d in range(1,31+1):
+        if csv_days[d] > csv_max: csv_max = csv_days[d]
+
+#    csv_max = max(csv_days)
+ 
+    count += csv_count
+
+    print(f"{zip_file :>35}:{csv_max: >3}|",end=" ")
+    for d in range(1,31+1): print(f"{csv_days[d]: >3}", end=" ")
+    print(f"|/{csv_count}")
+    #print(f"{zip_file: >35} : {csv_max} {csv_days}/{csv_count}")
+
+    return (days,max,count)
+
+def browse_zip_file(zip_file,csv_file,days,max,count):
+
+    """
+    Reads a ZIP file containing Zerto billing data and processes it to find the maximum number of days with recorded usage.
+
+    Parameters:
+    zip_file (str): Name of the ZIP file to read.
+    csv_file (str): Name of the CSV file to read in the ZIP file.
+    days (list): List of 33 integers where the index is the day of the month and the value is the count of days with usage.
+    max (int): The maximum number of days with recorded usage.
+
+    Returns:
+    tuple: days and max
+    """
+    try:
+        with ZipFile(zip_file,'r') as zip:
+            zip.extract(billing_file)
+            with ZipFile(billing_file,'r') as bill:
+                with bill.open(csv_file) as file:
+                    (days,max,count)=browse_csv_file(file,zip_file,days,max,count)
+    
+    except Exception as e:
+        print(f"Error extract {billing_file} or {csv_file} from {zip_file} : {e}")
+
+    return (days,max,count)
+
+def find_max_days(zip_files,csv_file,year,month):
+    """
+    Processes a list of Zerto ZIP files to determine the maximum number of days
+    with recorded usage within a specified month and year.
+
+    Parameters:
+    zip_files (list): A list of ZIP file paths to process.
+    csv_file (str): The name of the CSV file to extract and read from each ZIP file.
+    year (int): The year to process.
+    month (int): The month to process.
+
+    Returns:
+    None
+    """
+
+    days = [0] * 33
+    max=-1
+    max_day = []
+    count = 0
+    title = "################### Zerto CSV file"
+    total = "TOTAL"
+
+    print(f"{title :<35}:   |", end=" ")
+    for d in range(1,31+1): print(f"{d: >3}", end=" ")
+    print(f"|")
+    
+    for zip_file in zip_files:
+        (days,max,count) = browse_zip_file(zip_file,csv_file,days,max,count)
+
+    for d in range(1,31+1):
+        if days[d] > max: max = days[d]
+
+    for d in range(1,31+1):
+        if days[d] == max: max_day.append(d)
+
+    print(f"{total :>35}:   |", end=" ")
+    for d in range(1,31+1): print(f"{days[d]: >3}", end=" ")
+    print(f"|/{count}")
+
+    # os.remove(billing_file)
+
+    print(f"\nZerto usage for {month}-{year} ==> {max} {max_day}/{count}\n")
+
+def main():
+    """
+    Processes Zerto billing data in ZIP files to find the maximum number of days with recorded usage.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    today = dt.now()
+    parser = ap.ArgumentParser()
+    parser.add_argument('-m', '--month', type=int, default=today.month - 1, help='Indicate the month number (1 to 12) to process.')
+    parser.add_argument('-y', '--year', type=int, default=today.year, help='Indicate the year number (2025) to process.')
+    parser.add_argument('zip_files', nargs='+', help='List of Zerto zip files to process separated by space.')
+    args = parser.parse_args()
+
+    find_max_days(args.zip_files, f"ZertoBilling_{args.month}_{args.year}.csv", args.year, args.month)
+
+if __name__ == "__main__":
+    main()
