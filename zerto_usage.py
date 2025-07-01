a0e2589f53d251bb85b33c9662a47a94fc2bcaf5
Frederic Perreau <Frederic.Perreau@kyndryl.com>
2025-07-01 14:35:51 +0200

    resolve max() error because of existing max variable

--- zerto_usage.py
+++ zerto_usage.py
@@ -7,80 +7,70 @@ from zipfile import ZipFile
 
 billing_file = "ZertoBilling.zip"
 date_format = "%m/%d/%Y%H:%M:%S%p"
-
 row = ["file", "max", "count"] + ["d"+str(i+1) for i in range(0,31)]
 out = pd.DataFrame(columns=row)
 
-def browse_csv_file(file, zip_file, days, max, count):
+def browse_csv_file(file, zip_file, days, count):
     """
     Reads a CSV file containing Zerto billing data and processes it to find the maximum number of days with recorded usage.
 
     Parameters:
-    file (file): The file to read.
-    zip_file (str): The name of the ZIP file containing the CSV file.
-    days (list): List of 33 integers where the index is the day of the month and the value is the count of days with usage.
-    max (int): The maximum number of days with recorded usage.
-    count (int): The total count of VMs.
+    file (str): Name of the CSV file to read.
+    zip_file (str): Name of the ZIP file containing the CSV file.
+    days (list): List of 31 integers where the index is the day of the month and the value is the count of days with usage.
+    count (int): The total count of days with recorded usage.
 
     Returns:
-    tuple: days, max, and count
+    tuple: days and count
     """
-    csv_days = [0]*31
-    csv_max = -1
-
     df = pd.read_csv(file, skiprows=1, usecols=[" VM (Unique ID)", " From Date", " To Date", " Total Days"])
     df.rename(columns={' VM (Unique ID)': 'VM_Unique_ID'}, inplace=True)
     df.rename(columns={' From Date': 'From_Date'}, inplace=True)
     df.rename(columns={' To Date': 'To_Date'}, inplace=True)
     df.rename(columns={' Total Days': 'Total_Days'}, inplace=True)
  
+    csv_days = [0]*31
     csv_count = 0
+
     for row in df.itertuples():
         dFrom = dt.strptime(re.sub('[ ?]','',row.From_Date),date_format)
         dTo = dt.strptime(re.sub('[ ?]','',row.To_Date),date_format)
-
-        #print(dFrom.day,dTo.day,row.Total_Days,list(range(dFrom.day,dTo.day+1)))
         for d in range(dFrom.day,dTo.day+1):
             days[d-1] += 1
             csv_days[d-1] += 1
-
         csv_count += 1
-
-    for d in range(1,31+1):
-        if csv_days[d-1] > csv_max: csv_max = csv_days[d-1]
-
     count += csv_count
 
-    row = {"file": zip_file, "max": csv_max, "count": csv_count}
+    row = {"file": zip_file, "max": max(csv_days), "count": csv_count}
     row.update({"d"+str(index+1): element for index,element in enumerate(csv_days)})
     out.loc[len(out)] = row
 
-    return (days,max,count)
+    return (days,count)
 
-def browse_zip_file(zip_file,csv_file,days,max,count):
+def browse_zip_file(zip_file,csv_file,days,count):
     """
-    Reads a ZIP file containing Zerto billing data and processes it to find the maximum number of days with recorded usage.
+    Extracts and processes a CSV file from a ZIP file containing Zerto billing data.
 
     Parameters:
-    zip_file (str): Name of the ZIP file to read.
-    csv_file (str): Name of the CSV file to read in the ZIP file.
-    days (list): List of 33 integers where the index is the day of the month and the value is the count of days with usage.
-    max (int): The maximum number of days with recorded usage.
+    zip_file (str): The path to the main ZIP file containing the billing ZIP file.
+    csv_file (str): The name of the CSV file inside the billing ZIP file to be processed.
+    days (list): A list of 31 integers representing the days of the month with usage counts.
+    count (int): The total count of days with recorded usage.
 
     Returns:
-    tuple: days and max
+    tuple: Updated days and count after processing the CSV file.
     """
     try:
         with ZipFile(zip_file,'r') as zip:
             zip.extract(billing_file)
             with ZipFile(billing_file,'r') as bill:
                 with bill.open(csv_file) as file:
-                    (days,max,count)=browse_csv_file(file,zip_file,days,max,count)
+                    (days,count)=browse_csv_file(file,zip_file,days,count)
     
     except Exception as e:
         print(f"Error extract {billing_file} or {csv_file} from {zip_file} : {e}")
 
-    return (days,max,count)
+    return (days,count)
 
 def find_max_days(zip_files,csv_file,year,month):
     """
@@ -96,36 +86,31 @@ def find_max_days(zip_files,csv_file,year,month):
     Returns:
     None
     """
-
     days = [0] * 31
-    max=-1
-    max_day = []
+    max_days = []
     count = 0
     title = "################### Zerto CSV file"
     total = "TOTAL"
 
     for zip_file in zip_files:
-        (days,max,count) = browse_zip_file(zip_file,csv_file,days,max,count)
-
-    for d in range(1,31+1):
-        if days[d-1] > max: max = days[d-1]
-
-    for d in range(1,31+1):
-        if days[d-1] == max: max_day.append("d"+str(d))
-
+        (days,count) = browse_zip_file(zip_file,csv_file,days,count)
     os.remove(billing_file)
 
-    row = {"file": "TOTAL", "max": max, "count": count}
+    max_day = max(days)
+    for d in range(1,31+1):
+        if days[d-1] == max_day: max_days.append("d"+str(d))
+
+    row = {"file": "TOTAL", "max": max_day, "count": count}
     row2 = {"d"+str(index+1): element for index,element in enumerate(days)}
     row.update(row2)
     out.loc[len(out)] = row
 
-    print(f"{out}\n\nZerto usage for {month}-{year} ==> {max} {max_day}/{count}\n")
+    print(f"{out}\n\nZerto usage for {month}-{year} ==> {max_day} {max_days}/{count}\n")
     out.to_csv(f"{year}{month:02d}_Resiliency_France.csv",sep=';', encoding='utf-8',index=False, header=True)
 
 def main():
     """
-    Processes Zerto billing data in ZIP files to find the maximum number of days with recorded usage.
+    Main function to process a list of Zerto ZIP files to determine the maximum number of days with recorded usage within a specified month and year.
 
     Parameters:
     None
